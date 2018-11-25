3e83eb8bd16d524eda2fda6ccdf9667f883080b9
Dana Palmer <roiai4@hotmail.com>
2018-11-25 02:31:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,4 +27,18 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+
 end
