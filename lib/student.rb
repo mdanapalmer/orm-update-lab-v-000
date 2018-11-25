78f0a2e27a56f5234e4191204c1827c3910af46c
Dana Palmer <roiai4@hotmail.com>
2018-11-25 02:39:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,8 +41,8 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
-    DB[:conn].execute(sql, self.name, self.grade)
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
